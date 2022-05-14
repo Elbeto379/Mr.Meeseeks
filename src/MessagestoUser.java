845a79110f263bf2d7205db0de5a30ef0f20c8e0
Elbeto379 <67297348+Elbeto379@users.noreply.github.com>
2022-05-14 10:56:10 -0400

    creación de la clase MessagestoUser que retorna un mensaje de numero mayor o numero menor.

---
+++ src/MessagestoUser.java
@@ -0,0 +1,10 @@
+public class MessagestoUser {
+    //One method is declared to return a message in case user's number is major than the mysterious number
+    public final String getMajorNumberMessage(){
+        return "Your number is major than the mysterious number";
+    }
+    //One method is declared to return a message in case user's number is less than the mysterious number
+    public final String getLess_number_message(){
+        return "Your number is less than the mysterious number";
+    }
+}
